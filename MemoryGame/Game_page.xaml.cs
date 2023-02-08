08b7d16fad3ad493525d9401f72b9006ba929cd5
נאור ביטון <NAORAGAM0147@GMAIL.COM>
2023-02-08 19:30:38 +0200

    added photos

--- MemoryGame/Game_page.xaml.cs
+++ MemoryGame/Game_page.xaml.cs
@@ -11,6 +11,7 @@ using Windows.UI.Xaml.Controls.Primitives;
 using Windows.UI.Xaml.Data;
 using Windows.UI.Xaml.Input;
 using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Media.Imaging;
 using Windows.UI.Xaml.Navigation;
 
 // The Blank Page item template is documented at https://go.microsoft.com/fwlink/?LinkId=234238
@@ -22,18 +23,50 @@ namespace MemoryGame
     /// </summary>
     public sealed partial class Game_page : Page
     {
-        public int x=1;
+        public int GameTheme=1;//1 is Card 2 is football 
+        public int gameSize = 6;
+        
         public Game_page()
         {
             this.InitializeComponent();
-            Grid a = new Grid();
-            //my_G.RowDefinitions.Add
+
+            for (int i = 0; i < gameSize; i++)
+            {
+                RowDefinition rowDef = new RowDefinition();
+                my_G.RowDefinitions.Add(rowDef);
+                ColumnDefinition colDef = new ColumnDefinition();
+                my_G.ColumnDefinitions.Add(colDef);
+            }
+            Button button;
+            for (int i = 0; i < gameSize; i++)
+            {
+               /* button = new Button();
+                button.Content = GameTheme;
+                button.FontSize = 40;*/
+                
+
+                string _imageAdress = $"ms-appx:///football_players/Football_{i+1}.png";
+                Image a = new Image();
+                a.Source = new BitmapImage(new Uri(_imageAdress));
+                Grid.SetColumn(a, i);
+
+                
+                Grid.SetRow(a, i);
+                my_G.Children.Add(a);
+                
+            }
+          
+
+
 
         }
-
+        public string getPhoto()
+        {
+            return "Sdf";
+        }
         protected override void OnNavigatedTo(NavigationEventArgs e)
         {
-            x = (int)e.Parameter;
+            GameTheme = (int)e.Parameter;
         }
     }
 }
